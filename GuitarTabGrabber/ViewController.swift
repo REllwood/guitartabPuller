008f1c727df394b4b567da173958dd9f95d52d79
rhysellwood <s3491191@student.rmit.edu.au>
2017-10-27 17:30:48 +1100

    Initial Commit

---
+++ GuitarTabGrabber/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GuitarTabGrabber
+//
+//  Created by Rhys Ellwood on 27/10/17.
+//  Copyright © 2017 Rhys Ellwood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
